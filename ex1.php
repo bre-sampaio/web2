38817c773cf5d9922acddbe00c3d726ec8677bb0
bre-sampaio <47746715+bre-sampaio@users.noreply.github.com>
2019-02-26 14:24:53 -0300

    Add files via upload

---
+++ ex1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $vetor = array();
+            
+            for($i=0; $i < 50; $i++){
+                $vetor[$i] = 42;
+            }
+        
+             var_dump($vetor);
+             
+        ?>      
+    </body>
+</html>
